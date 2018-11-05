e39c4546dbbea869317417691afc40fd11178126
Cheng Vue <cvu3@ucsc.edu>
2018-11-05 22:29:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,7 @@ function addElementToEndOfArray(array, element) {
   return newArr;
 }
 
-function destructiveelyAddElementToEndOfArray(array, element) {
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array;
 }
